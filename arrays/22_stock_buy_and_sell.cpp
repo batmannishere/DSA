c1076e41ce2b6eb19b5a01a520867aabe182a878
batmannishere <pulkit20.pant@gmail.com>
2026-07-06 08:26:04 +0530

    Update array problem solutions

--- arrays/22_stock_buy_and_sell.cpp
+++ arrays/22_stock_buy_and_sell.cpp
@@ -9,16 +9,14 @@ Space Complexity: O(1)
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        int min =INT_MAX;
-        int maxprofit=INT_MIN;
-        int cost;
+        int n= prices.size();
+        int mini = prices[0];
+        int maxprofit=0; // to chck btw cost and profit
+        int cost; // this after buying and selling
         for(int i=0;i<n;i++){
-            if(prices[i]<min){
-                min=prices[i];
-            }
-            cost=prices[i]-min;
-            maxprofit=max(maxprofit,cost);
+           cost=prices[i]-mini; // checking what cost comes after buying and selling
+           maxprofit= max(cost,maxprofit); // checking
+           mini=min(mini,prices[i]);// we will check if mini should be updated before moving forward
         }
         return maxprofit;
     }
--- arrays/25_next_permutation.cpp
+++ arrays/25_next_permutation.cpp
@@ -11,23 +11,23 @@ public:
     void nextPermutation(vector<int>& nums) {
         int ind=-1;
         int n=nums.size();
-        for(int i=n-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){     // breaking point find krne ke liye as array decending order mei honi chaiye jaha pr bhi arr[i]<arr[i+1] woh breaking point hai
             if(nums[i]<nums[i+1]){
                 ind =i;
                 break;
             }
         }
         if(ind==-1){
-            reverse(nums.begin(),nums.end());
+            reverse(nums.begin(),nums.end()); // agar hame breaking point nhi mila puri array descending order mei hai toh reverse krdenge
             return ;
         }
-        for(int i=n-1;i>ind;i--){
+        for(int i=n-1;i>ind;i--){   // if milgya then we will loop from end as we have to find the index just greater than that and piche se array descending order mei hai
             if(nums[ind]<nums[i]){
                 swap(nums[i],nums[ind]);
                 break;
             }
         }
-        reverse(nums.begin()+ind+1,nums.end());
+        reverse(nums.begin()+ind+1,nums.end()); // array sort hogyi ab reverse krdenge
         return ;
 
     }

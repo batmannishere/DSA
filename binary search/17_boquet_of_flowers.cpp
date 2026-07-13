fbd95efa64ff3ac137aaad79e15e7c3c0566f2c8
batmannishere <pulkit20.pant@gmail.com>
2026-07-13 21:19:10 +0530

    Add binary search solutions

---
+++ binary search/17_boquet_of_flowers.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    bool possible(vector<int>& bloomDay, int day, int m, int k) {
+        int cnt = 0;
+        int bouquets = 0;
+
+        for (int flower : bloomDay) {
+            if (flower <= day) {
+                cnt++;
+                if (cnt == k) {
+                    bouquets++;
+                    cnt = 0;
+                }
+            } else {
+                cnt = 0;
+            }
+        }
+
+        return bouquets >= m;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long required = 1LL * m * k;
+
+        if (required > bloomDay.size()) return -1;
+
+        int low = *min_element(bloomDay.begin(), bloomDay.end());
+        int high = *max_element(bloomDay.begin(), bloomDay.end());
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (possible(bloomDay, mid, m, k)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+};

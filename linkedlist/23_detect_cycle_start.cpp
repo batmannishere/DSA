26051b342ab479bef6129374f8131f60744a4f80
batmannishere <pulkit20.pant@gmail.com>
2026-07-17 22:28:53 +0530

    Add linked list solutions

--- linkedlist/23_detect_cycle_start.cpp
+++ linkedlist/23_detect_cycle_start.cpp
@@ -1,23 +1,31 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
+ class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-        ListNode*slow=head ;
-        ListNode*fast=head;
-        while(fast!=nullptr && fast->next!=nullptr){
-             slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast){
-                return true;
+    ListNode *detectCycle(ListNode *head) {
+
+        if (head == nullptr || head->next == nullptr)
+            return nullptr;
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                slow = head;
+                break;
             }
         }
-        return false;
+
+        if (fast == nullptr || fast->next == nullptr)
+            return nullptr;
+
+        while (slow != fast) {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
     }
 };

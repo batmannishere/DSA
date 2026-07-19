c84fad63f34a4f7223738b6b4454cebdf6ff50ea
batmannishere <pulkit20.pant@gmail.com>
2026-07-19 22:06:04 +0530

    Add reverse linked list solution

---
+++ linkedlist/25_palindrome_linked_list.cpp
@@ -0,0 +1,41 @@
+// Node class represents a node in a linked list
+class Node {
+public:
+    int data;       // Data stored in the node
+    Node* next;     // Pointer to the next node in the list
+
+    // Constructor with both data and next node as parameters
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    // Constructor with only data as a parameter, sets next to nullptr
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+// Function to reverse a linked list using the recursive approach
+Node* reverseLinkedList(Node* head) {
+    // Check if the list is empty or has only one node
+    if (head == NULL || head->next == NULL) {
+        return head;  // No change is needed; return the current head
+    }
+
+    // Recursive step: Reverse the remaining part of the list and get the new head
+    Node* newHead = reverseLinkedList(head->next);
+
+    // Store the next node in 'front' to reverse the link
+    Node* front = head->next;
+
+    // Update the 'next' pointer of 'front' to point to the current head
+    front->next = head;
+
+    // Set the 'next' pointer of the current head to null to break the original link
+    head->next = NULL;
+
+    // Return the new head obtained from the recursion
+    return newHead;
+}
